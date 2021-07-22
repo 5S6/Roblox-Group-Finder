447ce38960fe8f2c2f02ff2180ce440e4b28da22
AlekDevs <amacwilliams06@gmail.com>
2021-07-21 22:03:53 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import os 
+import threading
+import requests, random
+from dhooks import Webhook
+
+
+def groupfinder():
+    id = random.randint(1000000, 1150000)
+    r = requests.get(f"https://www.roblox.com/groups/group.aspx?gid={id}") 
+    if 'owned' not in r.text:
+        re = requests.get(f"https://groups.roblox.com/v1/groups/{id}")
+        if 'isLocked' not in re.text and 'owner' in re.text:
+            if re.json()['publicEntryAllowed'] == True and re.json()['owner'] == None:
+                hook.send(f'Hit: https://www.roblox.com/groups/group.aspx?gid={id}')
+                print(f"[-] Hit: {id}")
+            else:
+                print(f"[-] No Entry Allowed: {id}")
+        else:
+            print(f"[-] Group Locked: {id}")
+    else:
+        print(f"[-] Group Already Owned: {id}")
+
+
+print("""
+
+
+____ _    ____ _  _ ____    ____ ____ ____ _  _ ___  
+|__| |    |___ |_/  [__     | __ |__/ |  | |  | |__] 
+|  | |___ |___ | \_ ___]    |__] |  \ |__| |__| |    
+                                                     
+____ _ _  _ ___  ____ ____ 
+|___ | |\ | |  \ |___ |__/ 
+|    | | \| |__/ |___ |  \ 
+                           
+""")
+
+hook = input("[-] Enter your webhook: ")
+threads = int(input("[-] How many threads: "))
+
+while True:
+    if threading.active_count() <= threads:
+        threading.Thread(target=groupfinder).start()
