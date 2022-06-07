275e20a2d6b65f327aa82ac884ca974c5d01a788
elgatokevin <107011256+elgatokevin@users.noreply.github.com>
2022-06-06 22:03:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def groupfinder():
     id = random.randint(1000000, 1150000)
     r = requests.get(f"https://www.roblox.com/groups/group.aspx?gid={id}") 
     if 'owned' not in r.text:
-        re = requests.get(f"https://groups.roblox.com/v1/groups/{id}")
+        re = requests.get(f"https://groups.roblox.com/v1/groups/20243-345354")
         if 'isLocked' not in re.text and 'owner' in re.text:
             if re.json()['publicEntryAllowed'] == True and re.json()['owner'] == None:
                 hook.send(f'Hit: https://www.roblox.com/groups/group.aspx?gid={id}')
